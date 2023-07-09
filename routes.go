e21e46a7244cd86c32ec7dddc7e09d014fef9f37
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2023-07-08 22:59:19 -0500

    remove unnecessary comment

--- routes.go
+++ routes.go
@@ -5,21 +5,14 @@ import "github.com/spf13/viper"
 func initializeRoutes(config *viper.Viper) {
 	router.Use(setUserStatus())
 
-	//router.POST("/api/login", generateToken)
 	router.GET("/api/users", viewUsers)
 	router.GET("/api/urls", viewURLs)
 	router.GET("/api/stats", viewStats)
 
 	router.GET("/u/:url", urlStats(), redirectShortURL)
 	router.GET("/", ensureNotLoggedIn(), showIndexPage)
-	router.POST("/u/shorturl", checkUserMiddleware(), shorturl)
+	router.POST("/u/shorturl", shorturl)
 	router.POST("/u/changelink", changeLink)
-	//router.POST("/login", login(config))
-	router.GET("/login", ensureNotLoggedIn(), showLoginPage)
-	//router.POST("/logout", TokenAuthMiddleware(config), logout(config))
-	router.GET("/register", ensureNotLoggedIn(), showRegistrationPage)
-	//router.POST("/register", ensureNotLoggedIn(), register(config))
-	router.GET("/session", checkSession)
 
 	// stats URLs
 	router.GET("/stats", showStatsPage(config))
--- shorten_url.go
+++ shorten_url.go
@@ -1,5 +1,3 @@
-// c.AbortWithError(http.StatusNotFound, err)
-// c.AbortWithStatus(http.StatusNotFound)
 package main
 
 import (
@@ -9,7 +7,6 @@ import (
 	"time"
 
 	"github.com/Showmax/go-fqdn"
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
@@ -29,37 +26,9 @@ func showIndexPage(c *gin.Context) {
 	)
 }
 
-func checkUserMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// session := sessions.Default(c)
-		// sessionID := session.Get("user_logged_in")
-
-		// if sessionID == nil {
-		// 	c.JSON(http.StatusNotFound, gin.H{"message": "unauthorized"})
-		// 	c.Abort()
-		// }
-	}
-}
-
 func shorturl(c *gin.Context) {
 	var url URL
 	_ = c.ShouldBind(&url)
-
-	// session := sessions.Default(c)
-	// userFound := session.Get("user_logged_in")
-
-	// if userFound == nil {
-	// 	c.HTML(
-	// 		http.StatusInternalServerError,
-	// 		"error5xx.html",
-	// 		gin.H{
-	// 			"title":             "Error",
-	// 			"error_description": `You have to be logged in.`,
-	// 		},
-	// 	)
-
-	// 	return
-	// }
 
 	id, _ := (*urlDAO).save(url)
 	shortURL := idToShortURL(id, chars)
@@ -144,97 +113,6 @@ func redirectShortURL(c *gin.Context) {
 	}
 }
 
-// func generateToken(c *gin.Context) {
-// 	type User struct {
-// 		ID       uint64 `json:"id,omitempty"`
-// 		Username string `json:"username"`
-// 		Password string `json:"password"`
-// 	}
-
-// 	dummyUser := User{
-// 		ID:       1,
-// 		Username: "username",
-// 		Password: "password",
-// 	}
-
-// 	var u User
-// 	if err := c.ShouldBindJSON(&u); err != nil {
-// 		c.JSON(http.StatusUnprocessableEntity, "invalid json provided")
-
-// 		return
-// 	}
-// 	// compare the user from the request, with the one we defined:
-// 	if dummyUser.Username != u.Username || dummyUser.Password != u.Password {
-// 		c.JSON(http.StatusUnauthorized, "please provide valid login details")
-
-// 		return
-// 	}
-
-// 	ts, err := CreateToken(dummyUser.ID, envConfig)
-// 	if err != nil {
-// 		c.JSON(http.StatusUnprocessableEntity, err.Error())
-
-// 		return
-// 	}
-
-// 	saveErr := CreateAuth(dummyUser.ID, ts)
-// 	if saveErr != nil {
-// 		c.JSON(http.StatusUnprocessableEntity, saveErr.Error())
-// 	}
-
-// 	tokens := map[string]string{
-// 		"access_token":  ts.AccessToken,
-// 		"refresh_token": ts.RefreshToken,
-// 	}
-// 	c.JSON(http.StatusOK, tokens)
-// }
-
-// func createTokenFromUser(userid string, config *viper.Viper) (*TokenDetails, error) {
-// 	td := &TokenDetails{}
-// 	td.AtExpires = time.Now().Add(TokenExpirationMinutes).Unix()
-
-// 	u, _ := uuid.NewV4()
-// 	td.AccessUUID = u.String()
-
-// 	td.RtExpires = time.Now().Add(Hours24).Unix()
-
-// 	var err error
-
-// 	atClaims := jwt.MapClaims{}
-// 	atClaims["access_uuid"] = td.AccessUUID
-// 	atClaims["user_id"] = userid
-// 	atClaims["exp"] = td.AtExpires
-// 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-
-// 	td.AccessToken, err = at.SignedString([]byte(config.GetString("secret")))
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error creating signed string: %v", err)
-// 	}
-
-// 	return td, nil
-// }
-
-// func logout(config *viper.Viper) gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		au, err := ExtractTokenMetadata(c.Request, config)
-// 		if err != nil {
-// 			c.JSON(http.StatusUnauthorized, "unauthorized")
-
-// 			return
-// 		}
-
-// 		deleted, delErr := DeleteAuth(au.AccessUUID)
-// 		if delErr != nil || deleted == 0 { // if any goes wrong
-// 			c.JSON(http.StatusUnauthorized, "unauthorized")
-
-// 			return
-// 		}
-
-// 		c.JSON(http.StatusOK, "Successfully logged out")
-// 	}
-
-// }
-
 // Render one of HTML, JSON or CSV based on the 'Accept' header of the request
 // If the header doesn't specify this, HTML is rendered, provided that
 // the template name is present.
@@ -255,107 +133,6 @@ func render(c *gin.Context, data gin.H, templateName string) {
 	}
 }
 
-func showLoginPage(c *gin.Context) {
-	render(c, gin.H{
-		"title": "Login",
-	}, "login.html")
-}
-
-func showRegistrationPage(c *gin.Context) {
-	render(c,
-		gin.H{
-			"title": "Register",
-		}, "register.html",
-	)
-}
-
-// func register(config *viper.Viper) gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		// Obtain the POSTed username and password values
-// 		username := c.PostForm("username")
-// 		password := c.PostForm("password")
-
-// 		if err := validateNewUserFields(username, password); err != nil {
-// 			c.HTML(http.StatusInternalServerError, "register.html", gin.H{
-// 				"ErrorTitle":   "Registration Failed",
-// 				"ErrorMessage": err.Error(),
-// 			})
-
-// 			return
-// 		}
-
-// 		hashPassword := hashAndSalt([]byte(password))
-
-// 		exists, err := (*userDAO).userExists(username)
-// 		if err != nil {
-// 			c.HTML(http.StatusInternalServerError, "register.html", gin.H{
-// 				"ErrorTitle":   "Registration Failed",
-// 				"ErrorMessage": err.Error(),
-// 			})
-
-// 			return
-// 		}
-
-// 		if exists {
-// 			c.HTML(http.StatusInternalServerError, "register.html", gin.H{
-// 				"ErrorTitle":   "Registration Failed",
-// 				"ErrorMessage": "User already exists",
-// 			})
-
-// 			return
-// 		}
-
-// 		newUser, err := (*userDAO).addUser(username, hashPassword)
-// 		if err != nil {
-// 			_, _ = fmt.Fprintf(os.Stderr, "Error: %v", err)
-// 			c.HTML(http.StatusInternalServerError, "register.html", gin.H{
-// 				"ErrorTitle":   "Registration Failed",
-// 				"ErrorMessage": "Error creating user, contact the administrator.",
-// 			})
-// 		}
-
-// 		token, err := CreateTokenString(&newUser, config)
-// 		if err != nil {
-// 			c.HTML(http.StatusInternalServerError, "register.html", gin.H{
-// 				"ErrorTitle":   "Registration Failed",
-// 				"ErrorMessage": err.Error(),
-// 			})
-
-// 			return
-// 		}
-
-// 		c.SetCookie("token", token, 3600, "", "", false, true)
-// 		c.Set("is_logged_in", true)
-
-// 		session := sessions.Default(c)
-// 		session.Set("user_logged_in", newUser)
-
-// 		if err := session.Save(); err != nil {
-// 			c.HTML(http.StatusInternalServerError, "register.html", gin.H{
-// 				"ErrorTitle":   "Registration Failed",
-// 				"ErrorMessage": "Error creating user, contact the administrator.",
-// 			})
-
-// 			return
-// 		}
-
-// 		render(c, gin.H{
-// 			"title": "Successful registration & Login",
-// 		}, "login-successful.html")
-// 	}
-// }
-
-func checkSession(c *gin.Context) {
-	session := sessions.Default(c)
-	userFound := session.Get("user_logged_in")
-
-	fmt.Println("debug session ... ")
-	fmt.Println(userFound)
-	fmt.Println("debug session ... end")
-
-	c.JSON(http.StatusOK, "OK...")
-}
-
 func viewUsers(c *gin.Context) {
 	users, err := (*userDAO).findAll()
 	if err != nil {
@@ -366,22 +143,6 @@ func viewUsers(c *gin.Context) {
 }
 
 func viewURLs(c *gin.Context) {
-	// session := sessions.Default(c)
-	// userFound := session.Get("user_logged_in")
-
-	// if userFound == nil {
-	// 	c.HTML(
-	// 		http.StatusInternalServerError,
-	// 		"error5xx.html",
-	// 		gin.H{
-	// 			"title":             "Error",
-	// 			"error_description": `You have to be logged in.`,
-	// 		},
-	// 	)
-
-	// 	return
-	// }
-
 	urls, err := (*urlDAO).findAllByUser()
 	if err != nil {
 		_ = c.AbortWithError(http.StatusInternalServerError, err)
