5d27aacaec28e4e513ca615f917c9e72cddb5ae8
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2023-07-08 23:05:14 -0500

    upgrade deps / remove unnecessary files / remove middleware functions

--- routes.go
+++ routes.go
@@ -3,14 +3,12 @@ package main
 import "github.com/spf13/viper"
 
 func initializeRoutes(config *viper.Viper) {
-	router.Use(setUserStatus())
-
 	router.GET("/api/users", viewUsers)
 	router.GET("/api/urls", viewURLs)
 	router.GET("/api/stats", viewStats)
 
 	router.GET("/u/:url", urlStats(), redirectShortURL)
-	router.GET("/", ensureNotLoggedIn(), showIndexPage)
+	router.GET("/", showIndexPage)
 	router.POST("/u/shorturl", shorturl)
 	router.POST("/u/changelink", changeLink)
 
